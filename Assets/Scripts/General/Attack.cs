0822efc7a5bdd4fc1de57d56e5a2780175cae585
naibusong <2862713706@qq.com>
2025-02-20 22:49:56 +0800

    角色三段攻击动画判定box

--- Assets/Scripts/General/Attack.cs
+++ Assets/Scripts/General/Attack.cs
@@ -9,7 +9,7 @@ public class Attack : MonoBehaviour
     public float attackRate;//频率
 
     private void OnTriggerStay2D(Collider2D other)
-    {
+    {//敌人和主角都使用了takedamege方法，攻击判定范围和主角有重合则自己会扣血，因此要改LayerMask
         other.GetComponent<Character>()?.TakeDamege(this);
     }
 }
